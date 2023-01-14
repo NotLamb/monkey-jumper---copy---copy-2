6da11349e0e651ab0f45728d7abd73a953808774
NotLamb <122663621+NotLamb@users.noreply.github.com>
2023-01-14 01:20:33 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+zenum ActionKind {
+    Walking,
+    Idle,
+    Jumping
+}
+enum RadioMessage {
+    message1 = 49434,
+    RESETGRAVITY = 50857,
+    ENABLEGRAVITY = 56972
+}
+scene.setBackgroundImage(null)
